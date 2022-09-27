9749e6a771b2827dfcdf7d599ea2f16453bd9e7e
C0B21103 <c0b2110319@edu.teu.ac.jp>
2022-09-27 17:51:17 +0900

    コメント#4実装しようとしてみましたが時間内にエラーのデバッグが終わりませんでした。ごめんなさい。

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,18 +1,19 @@
 import random
-print("対象文字:")
-chr_list = []
-for i in range(10):
-    chr_number = random.randint(65,90)
-    chr_list += chr(chr_number)
-    print(chr(chr_number), end = " ")
+def start():
+    print("対象文字:")
+    chr_list = []
+    for i in range(10):
+        chr_number = random.randint(65,90)
+        chr_list += chr(chr_number)
+        print(chr(chr_number), end = " ")
 
-print('\n')
-print("表示文字:")
-x = 9    
-for j in range(8):
-    list_number = random.randint(0,x)
-    x = x - 1
-    print(chr_list.pop(list_number), end = " ")
+    print('\n')
+    print("表示文字:")
+    x = 9    
+    for j in range(8):
+        list_number = random.randint(0,x)
+        x = x - 1
+        print(chr_list.pop(list_number), end = " ")
 
 print('\n')
 print(len(chr_list))
@@ -22,6 +23,8 @@ while (flag_num == 0):
     answer = input("欠損文字はいくつあるでしょうか？:")
     if (len(chr_list) == answer):
         input("正解です。では、その欠損文字は？:")
-        break
+        flag_num = 1
     else:
         print("不正解です。やりなおし。")
+
+start()
