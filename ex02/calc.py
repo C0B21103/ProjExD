734a33ff009325ca8eb9b7b308057a688338c4e3
C0B21103 <c0b2110319@edu.teu.ac.jp>
2022-10-04 14:47:14 +0900

    問4

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,6 +10,9 @@ def click_number(event):
 root = tk.Tk() # 練習1
 root.geometry("300x500")
 
+entry = tk.Entry(root, width = 10, font = ("Times New Roman", 40), justify = "right")
+entry.grid(row = 0, column = 0, columnspan= 3)
+
 r, c = 0, 0
 for i, num in enumerate(range(9,-1, -1), 1):
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
@@ -19,4 +22,6 @@ for i, num in enumerate(range(9,-1, -1), 1):
     if i%3 == 0:
         r += 1
         c = 0
+        
+
 root.mainloop()
