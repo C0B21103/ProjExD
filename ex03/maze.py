bb19bda68be0a28c0fab3f36a8e6a967b3d41cba
C0B21103 <c0b2110319@edu.teu.ac.jp>
2022-10-18 12:21:45 +0900

    2

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,4 +2,11 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("迷えるこうかとん")
+root.geometry("1500x900")
+#キャンバスエリア
+canvas = tk.Canvas(root, width = 1500, height = 900)
+#塗りつぶし
+canvas.create_rectangle(0, 0, 1500, 900, fill = 'black')
+#キャンバス配置
+canvas.place(x=0, y=0)
 root.mainloop()
