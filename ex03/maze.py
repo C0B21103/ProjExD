fe6d362793eeab21573e3373f9e3d7068b027f89
C0B21103 <c0b2110319@edu.teu.ac.jp>
2022-10-18 12:38:33 +0900

    3

--- ex03/maze.py
+++ ex03/maze.py
@@ -9,4 +9,12 @@ canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
 #canvas.create_rectangle(0, 0, 1500, 900, fill = 'black')
 #キャンバス配置
 canvas.place(x=0, y=0)
+
+#こうかとんの画像のインスタンスを生成
+tori = tk.PhotoImage(file = "ex03/fig/0.png")
+#グローバル変数cx,cyは，こうかとんの現在地を表す変数である
+#cxは横座標,cyは縦座標
+cx, cy = 300, 400
+#Canvasにおける横：300，縦：400䛾座標に表示
+canvas.create_image(cx, cy, image = tori, tag = "tori")
 root.mainloop()
