aa076f3b39412c55dcdd407b3a5ed6e23fe7ed80
C0B21103 <c0b2110319@edu.teu.ac.jp>
2022-10-25 14:13:37 +0900

    4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,9 +7,9 @@ def main():
     pg.display.set_caption("逃げろ!こうかとん")
     #screen = pg.display.get_surface()
     bg_sfc = pg.image.load("pg_bg.jpg")
-    bg_sfc = pg.transform.rotozoom(bg_sfc, 0, 1.0)
+    #bg_sfc = pg.transform.rotozoom(bg_sfc, 0, 1.0)
     bg_rct = bg_sfc.get_rect()
-    bg_rct.center = 700, 400
+    #bg_rct.center = 700, 400
     
     tori_sfc = pg.image.load("fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
@@ -20,10 +20,20 @@ def main():
         pg.time.wait(30)
         scrn_sfc.blit(bg_sfc, bg_rct)
         scrn_sfc.blit(tori_sfc, tori_rct)
+        pressed = pg.key.get_pressed()
+        
+        if pressed[K_UP]:
+            tori_rct.move_ip(0, -5)
+        elif pressed[K_DOWN]:
+            tori_rct.move_ip(0, 5)
+        elif pressed[K_LEFT]:
+            tori_rct.move_ip(-5, 0)
+        elif pressed[K_RIGHT]:
+            tori_rct.move_ip(5, 0)
         
         clock = pg.time.Clock()
         clock.tick(1000)
-        
+        pg.event.pump()
         for event in pg.event.get():
             if event.type == QUIT:
                 pg.quit()
