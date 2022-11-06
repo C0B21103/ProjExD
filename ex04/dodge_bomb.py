fbbf449faf5c15fdbf4624921ba3d6869dd59a1f
C0B21103 <c0b2110319@edu.teu.ac.jp>
2022-11-06 17:15:39 +0900

    5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,9 +1,12 @@
 from pygame.locals import *
 import pygame as pg
 import sys
+import random
+
 def main():
     pg.init()
     scrn_sfc = pg.display.set_mode((1500, 800))
+    scrn_rct = scrn_sfc.get_rect()
     pg.display.set_caption("逃げろ!こうかとん")
     #screen = pg.display.get_surface()
     bg_sfc = pg.image.load("pg_bg.jpg")
@@ -15,13 +18,26 @@ def main():
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
+    
+
+    bomb_sfc = pg.Surface((100, 100))
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    clock = pg.time.Clock()
     while(1):
         pg.display.update()
-        pg.time.wait(30)
+        #pg.time.wait(30)
+        
+        
+        #pg.display.update()
+        
         scrn_sfc.blit(bg_sfc, bg_rct)
         scrn_sfc.blit(tori_sfc, tori_rct)
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
         pressed = pg.key.get_pressed()
-        
         if pressed[K_UP]:
             tori_rct.move_ip(0, -5)
         elif pressed[K_DOWN]:
@@ -31,7 +47,7 @@ def main():
         elif pressed[K_RIGHT]:
             tori_rct.move_ip(5, 0)
         
-        clock = pg.time.Clock()
+        #終了時
         clock.tick(1000)
         pg.event.pump()
         for event in pg.event.get():
