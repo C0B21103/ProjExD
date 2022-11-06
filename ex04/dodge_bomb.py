d56391dce16d6b67ac7d6d1928aa903208577a04
C0B21103 <c0b2110319@edu.teu.ac.jp>
2022-11-06 22:35:04 +0900

    追加機能

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -27,9 +27,19 @@ def main():
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx = random.randint(0, scrn_rct.width)
     bomb_rct.centery = random.randint(0, scrn_rct.height)
+    #2つ目の爆弾
+    bomb2_sfc = pg.Surface((100, 100))
+    bomb2_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb2_sfc, (255, 0, 0), (10, 10), 10)
+    bomb2_rct = bomb2_sfc.get_rect()
+    bomb2_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb2_rct.centery = random.randint(0, scrn_rct.height)
     clock = pg.time.Clock()
     
-    vx, vy = +1, +1
+    vx1, vy1 = +1, +1
+    vx2, vy2 = +1, +1
+    
+    count = 0
     
     while(1):
         pg.display.update()
@@ -43,11 +53,27 @@ def main():
         #爆弾の移動
         #画面外に出ないように制御
         if((bomb_rct.left < scrn_rct.left) or (scrn_rct.right < bomb_rct.right)):
-            vx *= -1
+            vx1 *= -1.05
         if((bomb_rct.top < scrn_rct.top) or (scrn_rct.bottom < bomb_rct.bottom)):
-            vy *= -1
-        bomb_rct.move_ip(vx, vy)
+            vy1 *= -1.05
+        bomb_rct.move_ip(vx1, vy1)
         scrn_sfc.blit(bomb_sfc, bomb_rct)
+        
+        #2つ目の爆弾の移動
+        if(bomb2_rct.left < scrn_rct.left):
+            bomb2_rct.centerx += 1500
+        if (scrn_rct.right < bomb2_rct.right):
+            bomb2_rct.centerx -=1500
+        if(bomb2_rct.top < scrn_rct.top):
+            bomb2_rct.centery += 800
+        if(scrn_rct.bottom < bomb2_rct.bottom):
+            bomb2_rct.centery -=800
+        bomb2_rct.move_ip(vx2, vy2)
+        scrn_sfc.blit(bomb2_sfc, bomb2_rct)
+        
+        if(count == 10):
+            scrn_sfc.blit(bomb_sfc, bomb_rct)
+            count = 0
         
         
         #矢印キーによるこうかとんの移動
@@ -63,7 +89,7 @@ def main():
             tori_rct.move_ip(5, 0)
         
         #爆弾とこうかとんの衝突    
-        if tori_rct.colliderect(bomb_rct):
+        if ((tori_rct.colliderect(bomb_rct)) or tori_rct.colliderect(bomb2_rct)):
             return
         
         #終了時
